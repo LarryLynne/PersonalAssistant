b99698f3b07417509b21fd9a4a8fe2228e8d2829
LarryLynne <vovka.drunique@gmail.com>
2022-12-24 14:04:57 +0200

    more main

---
+++ messages.py
@@ -0,0 +1,19 @@
+messages = {
+    -2: '\033[96m' + "- Hello! Let's get started!"+ '\033[0m',
+    -1: '- Done',
+    0: '- Unknown command',
+    1: '- More arguments needed',
+    2: '- Too many arguments',
+    3: '- Incorrect phone',
+    4: '- Phone not found',
+    5: '- Good bye!',
+    6: '- How can I help you?',
+    7: '- There is no any records in phonebook',
+    8: '- Incorrect phone number',
+    9: '- Invalid date of birth',
+    10: '- User already exists',
+    11: '- User not found',
+    12: '- Address book not saved',
+    13: '- Cannot load address book',
+    14: '- Users not found'
+}
