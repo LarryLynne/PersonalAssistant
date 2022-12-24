37d5639e5539a9043b803b4cfa2201f6b4b8dc69
VadymBoyko <109297726+VadymBoyko@users.noreply.github.com>
2022-12-24 22:20:21 +0200

    add property tags and work with tags

--- Note.py
+++ Note.py
@@ -1,7 +1,24 @@
 class Note():
     def __init__(self, caption: str) -> None:
         self.caption = caption
-        self.text = None
+        self._text = None
+        self._tags = []
 
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, text):
+        self._text = text
+        r = text.split(' ')
+        self._tags = []
+        for i in r:
+            if i.startswith('#') and len(i) > 1:
+                if i not in self._tags:
+                    self._tags.append(i)
+    @property
+    def tags(self):
+        return self._tags
     def __str__(self) -> str:
-        return f"{self.caption} : {self.text}"
+        return f"{self.caption} : {self.text} : Tags: {self.tags}"
