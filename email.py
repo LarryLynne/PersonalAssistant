1cd9d245e8428cc43f9140ddea577d851e37a673
VadymBoyko <109297726+VadymBoyko@users.noreply.github.com>
2022-12-27 14:48:40 +0200

    add setters/getters

--- email.py
+++ email.py
@@ -1,17 +1,25 @@
-from field import Field
 import re
 from messages import messages
 
-class Email(Field):
-    def __init__(self, value: str) -> None:
+class Email:
+    def __init__(self, value: str):
+        self._value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
         if value:
             ph = re.findall(r"[A-z]+[.]*[\w]*[.]*[\w]+[@][a-z]+[.][a-z]{2,10}", value)
             if ph:
-                self.value = (str(value))
+                self._value = (str(value))
             else:
                 raise ValueError(messages.get(23))
         else:
-            self.value = (str(value))
+            self._value = (str(value))
 
     def __str__(self) -> str:
         return str(self.value)
